4b9cb021df8a267591ff3c15c5d12a76dfa6c3ea
Lawrence <Lawrence4code@gmail.com>
2018-09-02 17:33:06 +0530

    Testing heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ greeting: 'Hi There!' });
+  res.send({ greeting: 'Hi There, from Heroku.' });
 });
 
 const PORT = process.env.PORT || 5000;
