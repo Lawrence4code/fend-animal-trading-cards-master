56a3e74d777f1408ee894400e8ff163f99cf2ef0
Lawrence <Lawrence4code@gmail.com>
2018-09-03 16:23:46 +0530

    completed setting mongoDB with mongoose and created neccessary route and configured cookie handling

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport'); // not returning or assigning so just requiringt will ensure its executed
+
+// DB settings
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ greeting: 'Hi There, from Heroku.' });
-});
+// cookies configuration
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require('./routes/authRoutes')(app) // this refactor can be done but might be little confusing
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/authRoutes.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+
+// google oauth handler
+// 'google' does the trick here, its let the route to authenticate using certain strategy
+// this route will be intiated by the user
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
+
+  // this route is for google oauth return callback
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  // handling logout
+  // this route will be initiated by the user
+  app.get('/api/logout', (req, res) => {
+    console.log('Logged off');
+    req.logout();
+    res.send(req.user);
+  });
+
+  // handling existing user with the help of cookies provided at the tine of login
+  // this route will be initiated when???
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+    if (!req.user) {
+      console.log('Not user available with the cookie provided.');
+    }
+  });
+};
